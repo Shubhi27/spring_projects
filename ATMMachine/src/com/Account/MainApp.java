54300df817c8ed8b19c9a6967644fbdead162af5
Shubhi Giri <65133348+Shubhi27@users.noreply.github.com>
2022-03-29 13:51:55 +0530

    Update MainApp.java

--- ATMMachine/src/com/Account/MainApp.java
+++ ATMMachine/src/com/Account/MainApp.java
@@ -6,11 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		 ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		 WithdrawalEventPublisher publisher = (WithdrawalEventPublisher) context.getBean("customEventPublisher");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		WithdrawalEventPublisher publisher = (WithdrawalEventPublisher) context.getBean("customEventPublisher");
 		BankAccount account = new SavingsAccount(100, "Shubhi", 50000);
-		double balance =account.withdraw(1000);
-		publisher.publish(balance,account);
+		 account.withdraw(1000);
+		publisher.publish(1000,account);
 	}
 
 }
+
